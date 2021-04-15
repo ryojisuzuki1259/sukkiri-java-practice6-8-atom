8bb6aa784880587db392bc517f1bd5df30f5bf50
鈴木良治 <suzukiryoji@suzukiyoshiharunoMacBook-Air.local>
2021-04-15 16:00:21 +0900

    Add codes

--- comment/Kouhan.java
+++ comment/Kouhan.java
@@ -4,9 +4,10 @@ public class Kouhan {
 	public static void callDeae() {
 		System.out.println("えぇい、こしゃくな。くせ者だ！であえい！");
 	}
-	public static void showMondokoro() {
+	public static void showMondokoro() throws Exception{
 		System.out.println("飛車さん、角さん。もういいでしょう。");
 		System.out.println("この紋所が目に入らぬか！");
+    Thread.sleep(3000);
 		Zenhan.doTogame();
 	}
 }

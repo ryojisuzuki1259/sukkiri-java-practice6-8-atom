2ddce24dc8511447f6de250b4f7f470ef5890c66
鈴木良治 <suzukiryoji@suzukiyoshiharunoMacBook-Air.local>
2021-04-15 15:39:46 +0900

    First commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+import comment.Zenhan;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Zenhan.doWarusa();
+		Zenhan.doTogame();
+		comment.Kouhan.callDeae();
+		comment.Kouhan.showMondokoro();
+	}
+
+
+}
